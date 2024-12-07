4114c5b77e11fbf18cb60760648abf380bcf551f
NegosSolutions <negossoluciones@gmail.com>
2024-12-07 13:31:58 -0600

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// Selecciona el elemento input con la clase correcta
+var check = document.querySelector(".toggle-state");
+
+// Verifica si el elemento existe antes de agregar el evento
+if (check) {
+    check.addEventListener('click', idioma);
+}
+
+function idioma() {
+    // Comprueba si el checkbox está marcado
+    let id = check.checked;
+    if (id) {
+        // Redirige al usuario a la página deseada
+        location.href = "esindex.html";
+    }else{
+        location.href = "index.html";
+    }
+}
